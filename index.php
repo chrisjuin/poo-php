91f3e3e1db638bf1885d60c9ca440f27ee8c841a
chrisjuin <c.juin.cj@gmail.com>
2017-09-29 14:42:04 +0200

    modif

--- index.php
+++ index.php
@@ -1,12 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+
+class Perso 
+{
+    $Perso1 = new Perso ;
+    $Perso2 = new Perso ;
     
-</body>
-</html>
+    $Perso1->frapper($Perso2);
+    $Perso1->gagnerExperience();
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//creation de la classe "personnage"
+
+//class perso
+//{
+    
+    
+    // private $_force = 57;
+    //private $_localisation = 'cosmos'; 
+    //private $_experience = 'expert'; 
+    //private $_degats = 78; 
+    
+    //private $_Experience = '20'; 
+    
+    
+    //public function afficherExperience()
+    //{
+        //  echo $this->_Experience; 
+        //}
+        
+        
+        //  public function gagnerExperience()
+        //{
+            //on ajoute 1 a notre attribut $_experience
+            //$this->_Experience = $this->_Experience + 1;
+            //}
+            
+            //}
+            
+            
+            
+            //$perso = new perso; 
+            // $perso->gagnerExperience();
+            //$perso->afficherExperience(); 
+            
+            
+            
+            
+            //$perso = new Personnage;
+            //$perso->_experience = $perso->_experience + 1; //  erreur fatale suite à cette instruction.
+            
+            
+            
+            
+            //déclaration d'une méthode dont le but est d'afficher du text 
+            
+            //public function parler()
+            //{
+                //echo "double cio " ;
+                //}
+                
+                //}
+                
+                //$perso = new Perso; 
+                
+//$perso->parler(); 
+
+// on va chercher l'objet $perso et on invoque la méthode parler() sur l'objet avec '->'
+
+?>
+
+
+
+
+
+
+
+
+
+
+
