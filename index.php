8f38bbd914495903e04205a643cfbef314b6c1b9
chrisjuin <c.juin.cj@gmail.com>
2017-10-03 11:09:37 +0200

    modif

--- index.php
+++ index.php
@@ -1,93 +1,49 @@
-<?php
+<?php    
     
-    //creation de la classe "Perso"
-    
-    class Voiture 
-
-    {
-        private $_contenuReservoir = 0; 
-        private $_indicateurReservoirRouge = true;
+    class Voiture {
         
-        //Méthode
-
-        public function add contenuReservoir()
-            {
-            $this ->_contenuReservoir += $contenu; 
-            if ($this ->_contenuReservoir < 20) {          
-                $this ->_indicateurReservoirRouge = true; 
-            } else $this ->_indicateurReservoirRouge = false; 
-
-            }
-
-            
+        //proprietes = variables
+        private $indicateurReserve;
+        private $reservoir;
+        
+        //Constructor
+    
+        public function __construct($nbLitre){
+            $this->reservoir = $nbLitre;
+            //faire l'affectation de $indicateurReserve
+            //TRUE si $reservoir < 20, sinon FALSE
+            $this->setReservoir($nbLitre);
         }
-
-    $Voiture = new Voiture;
-    $Voiture->contenuReservoir();
-    
-
-    // on va chercher l'objet ($Perso) et on invoque la méthode public function "dommages"() sur l'objet avec '->'
-    //$this représente l'objet que l'on utilise ($Perso)  
-    
-    
-    
-    
-    
-
-
-//class perso
-//{
-    
-    
-    // private $_force = 57;
-    //private $_localisation = 'cosmos'; 
-    //private $_experience = 'expert'; 
-    //private $_degats = 78; 
-    
-    //private $_Experience = '20'; 
-    
-    
-    //public function afficherExperience()
-    //{
-        //  echo $this->_Experience; 
-        //}
         
+        //Accesseurs pour acceder en lecture (Getter) ou en ecriture (Setter) aux attributs:
+        //get pour obtenir une valeur, set c'est pour lui donner une valeur
+        public function getIndicateurReserve(){
+            return $this->indicateurReserve;
+        }
         
-        //  public function gagnerExperience()
-        //{
-            //on ajoute 1 a notre attribut $_experience
-            //$this->_Experience = $this->_Experience + 1;
-            //}
-            
-            //}
-            
-            
-            
-            //$perso = new perso; 
-            // $perso->gagnerExperience();
-            //$perso->afficherExperience(); 
-            
-            
-            
-            
-            //$perso = new Personnage;
-            //$perso->_experience = $perso->_experience + 1; //  erreur fatale suite à cette instruction.
-            
-            
-            
-            
-            //déclaration d'une méthode dont le but est d'afficher du text 
-            
-            //public function parler()
-            //{
-                //echo "double cio " ;
-                //}
-                
-                //}
-                
-                //$perso = new Perso; 
-                
-//$perso->parler(); 
+        public function setIndicateurReserve($indicateur){
+            $this->indicateurReserve = $indicateur;
+        }
+        
+        //Accesseur $reservoir
+        public function getReservoir(){
+            return $this->reservoir;
+        }
+        
+        public function setReservoir($nbLitre){
+            $this->reservoir = $nbLitre;
+            if ($nbLitre >= 20){
+                $this->setIndicateurReserve(FALSE);
+                echo "on peut rouler !";
+            }else{
+                $this->setIndicateurReserve(TRUE);
+                echo "Attention ! ";
+            }
+        }
+    }
+
+    $nbLitre = 25;
+    $maVoiture = new Voiture($nbLitre);
 
 
 ?>
